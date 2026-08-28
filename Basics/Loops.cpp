bbcdffc4d0dcb6b48696b396f14b1910764ce258
Prasad Chimkar <chimkarprasad@gmail.com>
2026-08-28 17:19:03 +0530

    Create Loops.cpp with loop examples
    Add examples of for and while loops in C++.

---
+++ Basics/Loops.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // For loop
+    cout << "For Loop:" << endl;
+
+    for (int i = 1; i <= 5; i++) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    // While loop
+    cout << "While Loop:" << endl;
+
+    int i = 1;
+
+    while (i <= 5) {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+}
