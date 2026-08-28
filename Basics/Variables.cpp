e5ba6b32b4874a5675850027ba633e3a50f2676e
Prasad Chimkar <chimkarprasad@gmail.com>
2026-08-28 17:17:47 +0530

    Add Variables.cpp to demonstrate variable usage

---
+++ Basics/Variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 19;
+    float marks = 85.5;
+    char grade = 'A';
+    string name = "Prasad";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
